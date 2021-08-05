7e831552b21d750c4dcd935282a2d11d530351fa
KrishnaPriya2208 <87087758+KrishnaPriya2208@users.noreply.github.com>
2021-08-05 12:54:38 +0530

    Add files via upload

--- coinGame.js
+++ coinGame.js
@@ -4,69 +4,89 @@ function isTouching(a, b) {
   const bRect = b.getBoundingClientRect();
 
   return !(
-   aRect.top + aRect.height < bRect.top ||
-   aRect.top > bRect.top + bRect.height ||
-   aRect.left + aRect.width < bRect.left ||
-   aRect.left > bRect.left + bRect.width)
-};
+    aRect.top + aRect.height < bRect.top ||
+    aRect.top > bRect.top + bRect.height ||
+    aRect.left + aRect.width < bRect.left ||
+    aRect.left > bRect.left + bRect.width
+  );
+}
 
-const avatar = document.querySelector('#player');
-const coin = document.querySelector('#coin');
-
+const avatar = document.querySelector("#player");
+const coin = document.querySelector("#coin");
+console.log(avatar.width, avatar.height);
 // var music = document.getElementById("idAudio");
 // music.play();
 
 // var BGM = new Audio('Collide.mp3');
 // BGM.play();
-
-
-window.addEventListener('keyup', function(e) {
-  if(e.key === 'ArrowDown' || e.key === 'Down') {
+let currLeft, currTop;
+window.addEventListener("keyup", function (e) {
+  const avadimensions = avatar.getBoundingClientRect();
+  console.log(avadimensions);
+  if (e.key === "ArrowDown" || e.key === "Down") {
+    // console.log(currTop, window.innerHeight, avadimensions.top);
+    if (currTop + avatar.height >= window.innerHeight && e.key === "ArrowDown")
+      return;
     moveVertical(avatar, 50);
     playMusic();
   }
-  else if (e.key === 'ArrowUp' || e.key === 'Up') {
-    moveVertical(avatar,-50);
+  else if (e.key === "ArrowUp" || e.key === "Up") {
+    if (currTop - avatar.height / 2 <= 0 && e.key === "ArrowUp") return;
+    // console.log(currTop);
+    // console.log(currTop + avatar.height / 2);
+    moveVertical(avatar, -50);
     playMusic();
   }
-  else if (e.key === 'ArrowRight' || e.key === 'Right') {
+  else if (e.key === "ArrowRight" || e.key === "Right") {
+    console.log(avatar.width, window.innerWidth);
+    if (currLeft + avatar.width >= window.innerWidth && e.key === "ArrowRight") return;
     moveHorizontal(avatar, 50);
-    avatar.style.transform = 'scale(1,1)';
+    avatar.style.transform = "scale(1,1)";
     playMusic();
   }
-  else if (e.key === 'ArrowLeft' || e.key === 'Left') {
-    moveHorizontal(avatar,-50);
-    avatar.style.transform = 'scale(-1,1)';
+  else if (e.key === "ArrowLeft" || e.key === "Left") {
+    if (currLeft <= 0 && e.key === "ArrowLeft") return;
+    moveHorizontal(avatar, -50);
+    avatar.style.transform = "scale(-1,1)";
     playMusic();
   }
-if (isTouching(avatar, coin)) moveCoin();
+
+
+  if (isTouching(avatar, coin)) moveCoin();
 });
 
 const moveVertical = (element, amount) => {
-  const currTop = extractPos(element.style.top);
+  currTop = extractPos(element.style.top);
+  console.log(currTop);
   element.style.top = `${currTop + amount}px`;
 };
 
 const moveHorizontal = (element, amount) => {
-  const currLeft = extractPos(element.style.left);
+  currLeft = extractPos(element.style.left);
   element.style.left = `${currLeft + amount}px`;
 };
 
 const playMusic = () => {
   var music = document.getElementById("idAudio");
   music.play();
+  music.addEventListener("ended", function () {
+    this.currentTime = 0;
+    this.play();
+  });
 };
 
 const extractPos = (pos) => {
-  if(!pos) return 100;
-  return parseInt(pos.slice(0,-2));
+  if (!pos) return 100;
+  return parseInt(pos.slice(0, -2));
 };
+
+console.log(coin.width);
 
 const moveCoin = () => {
   const x = Math.floor(Math.random() * window.innerWidth);
   const y = Math.floor(Math.random() * window.innerHeight);
+  console.log(x, y);
   coin.style.top = `${y}px`;
   coin.style.left = `${x}px`;
 };
-
 moveCoin();
